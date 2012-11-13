b8e932b87182afe3494fca9aa8dd52765e216e91
Josh S. Franz <josh@puzzleflow.us>
2012-11-13 14:45:02 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'aa185dcbff9c5547d92a70b01ce330d2eab17510cde21a644f72fe5942c5dd52ef4131ae3e1248af837de508b980cb018a924ba756368d07cd48030d2e5eb1aa'
