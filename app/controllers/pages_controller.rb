4db6f06cf5baab640e052eb5f44f867a876c0b4b
Josh S. Franz <josh@puzzleflow.us>
2012-11-15 14:30:25 -0500

    Done with Static Pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,21 @@
 class PagesController < ApplicationController
+
   def home
+    @title_standard = "Ruby on Rails Tutorial Sample App | "
+	@title_personal = "Thank you for visiting | "
+  
+  
+	@title_start = @title_standard
+	@title = "Home"
   end
 
   def contact
+	@title_start = @title_personal
+	@title = "Contact"
+  end
+  
+  def about
+	@title_start = @title_standard
+	@title = "About"
   end
 end
